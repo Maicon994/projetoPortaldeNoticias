b53f748695badcb40d9a9f49b6d92214cd3addd2
Maicon994 <maiconmachado.alunosaolucas@gmail.com>
2026-06-27 14:34:01 -0300

    Subindo correções e novas implementações.

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>The Fake Times - Portal Satírico</title>
     <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <header>
@@ -12,7 +13,7 @@
     <nav>
         <a href="index.php">Início</a>
         <?php if(isset($_SESSION['user_id'])): ?>
-            <a href="nova-noticia.php">✍️ Publicar Notícia</a>
+            <a href="nova-noticia.php">Publicar Notícia</a>
             <a href="perfil.php">👤 Meu Perfil (<?= htmlspecialchars($_SESSION['user_nome']) ?>)</a>
             <a href="logout.php" style="color: var(--accent)">Sair</a>
         <?php else: ?>
